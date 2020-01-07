0011d9551f5f6db656aa0d7032c2bd4df5ed2279
shenzhaonan <>
2020-01-07 10:18:51 +0800

    add MD5Tools add UnityWebTools

---
+++ MD5Tools.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Szn.Framework.UtilPackage
+{
+    public static class MD5Tools
+    {
+        private static readonly MD5CryptoServiceProvider _md5;
+
+        static MD5Tools()
+        {
+            _md5 = new MD5CryptoServiceProvider();
+        }
+
+        public static string GetStringMd5(string InString)
+        {
+            return GetBytesMd5(Encoding.UTF8.GetBytes(InString));
+        }
+
+        public static string GetFileMd5(string InFilePath)
+        {
+            if (!string.IsNullOrEmpty(InFilePath) && File.Exists(InFilePath))
+            {
+                return GetBytesMd5(File.ReadAllBytes(InFilePath));
+            }
+
+            return string.Empty;
+        }
+
+        public static string GetBytesMd5(byte[] InBytes)
+        {
+            byte[] hashBytes = _md5.ComputeHash(InBytes);
+            int hashBytesLength = hashBytes.Length;
+            StringBuilder sb = new StringBuilder(32);
+            for (int i = 0; i < hashBytesLength; i++)
+            {
+                sb.Append(hashBytes[i].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ UnityWebTools.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Szn.Framework.Web
+{
+    public static class UnityWebTools
+    {
+        public static void DownloadHandle(this MonoBehaviour InMono, string InUrl,
+            Action<bool, DownloadHandler, string> InResultCallback)
+        {
+            if (string.IsNullOrEmpty(InUrl))
+            {
+                Debug.LogError("Download url can not be null or empty.");
+                return;
+            }
+
+            if (null == InResultCallback)
+            {
+                Debug.LogError("Do not download unused resources.");
+                return;
+            }
+
+            if (null == InMono) DownloadHandle(InUrl, InResultCallback);
+            else InMono.StartCoroutine(DownloadHandleAsync(InUrl, InResultCallback));
+        }
+
+        public static IEnumerator DownloadHandleAsync(string InUrl,
+            Action<bool, DownloadHandler, string> InResultCallback)
+        {
+            if (null == InResultCallback)
+            {
+                Debug.LogError("Do not download unused resources.");
+                yield break;
+            }
+
+            using (UnityWebRequest request = UnityWebRequest.Get(InUrl))
+            {
+                yield return request.SendWebRequest();
+
+                if (request.isHttpError || request.isNetworkError) InResultCallback.Invoke(false, null, request.error);
+
+                InResultCallback.Invoke(true, request.downloadHandler, null);
+            }
+        }
+
+        public static void DownloadHandle(string InUrl, Action<bool, DownloadHandler, string> InResultCallback)
+        {
+            if (null == InResultCallback)
+            {
+                Debug.LogError("Do not download unused resources.");
+                return;
+            }
+
+            using (UnityWebRequest request = UnityWebRequest.Get(InUrl))
+            {
+                request.SendWebRequest();
+
+                while (!request.isDone)
+                {
+                }
+
+                if (request.isHttpError || request.isNetworkError) InResultCallback.Invoke(false, null, request.error);
+
+                InResultCallback.Invoke(true, request.downloadHandler, null);
+            }
+        }
+    }
+}
